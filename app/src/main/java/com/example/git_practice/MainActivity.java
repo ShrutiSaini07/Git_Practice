d04e93d2da1928ec5927a082b94902a4387cfe36
Hp <shrutisaini1415@gmail.com>
2023-06-12 00:13:47 +0530

    Revert "third commit"
    This reverts commit febd344e7330efbf876eaceff8dd1426b1dc0100.

--- app/src/main/java/com/example/git_practice/MainActivity.java
+++ app/src/main/java/com/example/git_practice/MainActivity.java
@@ -14,6 +14,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         println("Hiii!!");
-        println("Hiii!!")
+        print(ndcsjj)
     }
 }
