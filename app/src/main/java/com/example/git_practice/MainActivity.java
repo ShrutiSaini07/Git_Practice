7830f1b3d4c674861afa4e49b97062392e97f310
Hp <shrutisaini1415@gmail.com>
2023-06-12 00:35:54 +0530

    Experimental commit

--- app/src/main/java/com/example/git_practice/MainActivity.java
+++ app/src/main/java/com/example/git_practice/MainActivity.java
@@ -14,6 +14,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         println("Hiii!!");
-        prit();
+        println("Shruti");
     }
 }
